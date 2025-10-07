7baf7006e2b1cd274a3e05c44ad4bc8f8b81183e
datnhhe172337 <datnhhe172337@fpt.edu.vn>
2025-10-07 15:19:25 +0700

    update gitIgnore,  add folder configurations

---
+++ EduMatch.PresentationLayer/Configurations/CorsConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace EduMatch.PresentationLayer.Configurations
+{
+	public static class CorsConfiguration
+	{
+		public static IServiceCollection AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+		{
+			var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy", builder =>
+				{
+					builder.WithOrigins(allowedOrigins)
+						   .AllowAnyHeader()
+						   .AllowAnyMethod()
+						   .AllowCredentials();
+				});
+			});
+
+			return services;
+		}
+	}
+}
---
+++ EduMatch.PresentationLayer/Configurations/JwtConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EduMatch.PresentationLayer.Configurations
+{
+	public static class JwtConfiguration
+	{
+		public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+		{
+			var jwtSettings = configuration.GetSection("JwtSettings");
+			var secretKey = jwtSettings["SecretKey"];
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(options =>
+			{
+				options.RequireHttpsMetadata = false;
+				options.SaveToken = true;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuer = true,
+					ValidIssuer = jwtSettings["Issuer"],
+
+					ValidateAudience = true,
+					ValidAudience = jwtSettings["Audience"],
+
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+
+					ValidateLifetime = true
+				};
+			});
+
+			return services;
+		}
+	}
+}
