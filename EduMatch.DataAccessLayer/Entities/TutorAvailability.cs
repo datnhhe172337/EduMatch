c59fa15bb942f5f9c6aa627fb17243b6a95dde26
datnhhe172337 <datnhhe172337@fpt.edu.vn>
2025-10-07 14:27:09 +0700

    Clean tracked files according to .gitignore and update igignore

---
+++ EduMatch.DataAccessLayer/Entities/TutorAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EduMatch.DataAccessLayer.Entities;
+
+public partial class TutorAvailability
+{
+    public int AvailabilityId { get; set; }
+
+    public int TutorId { get; set; }
+
+    public byte DayOfWeek { get; set; }
+
+    public int SlotId { get; set; }
+
+    public bool IsRecurring { get; set; }
+
+    public DateOnly EffectiveFrom { get; set; }
+
+    public DateOnly? EffectiveTo { get; set; }
+
+    public virtual TimeSlot Slot { get; set; } = null!;
+
+    public virtual TutorProfile Tutor { get; set; } = null!;
+}
