bd089d190767a10ced96d6c6ae1617aad9facf30
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-06 21:37:38 +0700

    add AdminWalletService

---
+++ EduMatch.BusinessLogicLayer/Interfaces/IAdminWalletService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduMatch.BusinessLogicLayer.Interfaces
+{
+    internal class IAdminWalletService
+    {
+    }
+}
