b8b081066d68b9311138450819cd38b5f1715968
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-12 01:52:48 +0700

    add INotificationPusher

--- EduMatch.BusinessLogicLayer/Interfaces/INotificationService.cs
+++ EduMatch.BusinessLogicLayer/Interfaces/INotificationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EduMatch.BusinessLogicLayer.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace EduMatch.BusinessLogicLayer.Interfaces
 {
-    internal class INotificationService
+    public interface INotificationService
     {
+        // Internal method
+        Task CreateNotificationAsync(string userEmail, string message, string linkUrl = null);
+
+        Task<IEnumerable<NotificationDto>> GetNotificationsForUserAsync(string userEmail, int page, int pageSize);
+        Task<bool> MarkAsReadAsync(int notificationId, string userEmail);
+        Task<bool> MarkAllAsReadAsync(string userEmail);
+        Task<bool> DeleteNotificationAsync(int notificationId, string userEmail);
+        Task<int> GetUnreadNotificationCountAsync(string userEmail);
     }
 }
