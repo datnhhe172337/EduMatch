4afd787c5f9a49f7fb3786de8e6576681b547fc1
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-16 02:30:42 +0700

    update ireportrepo

--- EduMatch.DataAccessLayer/Interfaces/IReportRepository.cs
+++ EduMatch.DataAccessLayer/Interfaces/IReportRepository.cs
@@ -13,5 +13,6 @@ namespace EduMatch.DataAccessLayer.Interfaces
         Task<List<Report>> GetAllAsync();
         Task<Report> UpdateAsync(Report report);
         Task DeleteAsync(Report report);
+        Task<bool> ExistsSimilarReportAsync(string reporterEmail, string reportedEmail, string reason, int? excludeReportId = null, int lookbackDays = 7);
     }
 }
