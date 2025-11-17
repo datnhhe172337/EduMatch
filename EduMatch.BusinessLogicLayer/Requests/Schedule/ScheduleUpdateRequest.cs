fac9022a2b432e217ca55df4544a70a44c020ecf
datnhhe172337 <datnhhe172337@fpt.edu.vn>
2025-11-18 01:11:19 +0700

    Update ScheduleUpdateRequest.cs

--- EduMatch.BusinessLogicLayer/Requests/Schedule/ScheduleUpdateRequest.cs
+++ EduMatch.BusinessLogicLayer/Requests/Schedule/ScheduleUpdateRequest.cs
@@ -10,8 +10,6 @@ namespace EduMatch.BusinessLogicLayer.Requests.Schedule
         public int Id { get; set; }
         public int? AvailabilitiId { get; set; }
         public int? BookingId { get; set; }
-        [EnumDataType(typeof(ScheduleStatus), ErrorMessage = "Status phải là giá trị hợp lệ của ScheduleStatus")]
-        public ScheduleStatus? Status { get; set; }
         public string? AttendanceNote { get; set; }
         public bool? IsRefunded { get; set; }
 
