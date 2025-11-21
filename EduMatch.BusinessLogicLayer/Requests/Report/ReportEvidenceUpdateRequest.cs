490b2b603fde3e7f27a148c5cb1bb34bf714d421
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-21 22:03:20 +0700

    add  ReportEvidenceUpdateRequest

---
+++ EduMatch.BusinessLogicLayer/Requests/Report/ReportEvidenceUpdateRequest.cs
@@ -0,0 +1,20 @@
+using EduMatch.DataAccessLayer.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduMatch.BusinessLogicLayer.Requests.Report
+{
+    public class ReportEvidenceUpdateRequest
+    {
+        public MediaType? MediaType { get; set; }
+
+        [Url]
+        [StringLength(500)]
+        public string? FileUrl { get; set; }
+
+        [StringLength(255)]
+        public string? FilePublicId { get; set; }
+
+        [StringLength(255)]
+        public string? Caption { get; set; }
+    }
+}
