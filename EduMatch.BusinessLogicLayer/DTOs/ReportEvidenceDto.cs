6abdbb55feca28bdc88fe1ff073c792f6e74170c
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-22 04:58:42 +0700

    update reportEvidenceSto

--- EduMatch.BusinessLogicLayer/DTOs/ReportEvidenceDto.cs
+++ EduMatch.BusinessLogicLayer/DTOs/ReportEvidenceDto.cs
@@ -9,6 +9,7 @@ namespace EduMatch.BusinessLogicLayer.DTOs
         public int ReportId { get; set; }
         public string? SubmittedByEmail { get; set; }
         public MediaType MediaType { get; set; }
+        public ReportEvidenceType EvidenceType { get; set; }
         public string FileUrl { get; set; } = string.Empty;
         public string? FilePublicId { get; set; }
         public string? Caption { get; set; }
