fb8f36bd087054bc5350650670ca05a797e12e5f
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-28 19:24:08 +0700

    update iadminstat

--- EduMatch.BusinessLogicLayer/Interfaces/IAdminStatsService.cs
+++ EduMatch.BusinessLogicLayer/Interfaces/IAdminStatsService.cs
@@ -9,5 +9,6 @@ namespace EduMatch.BusinessLogicLayer.Interfaces
         Task<AdminSummaryStatsDto> GetSummaryAsync(DateTime? fromUtc = null, DateTime? toUtc = null);
         Task<IReadOnlyList<SignupTrendPointDto>> GetSignupTrendAsync(DateTime? fromUtc = null, DateTime? toUtc = null, string groupBy = "day");
         Task<IReadOnlyList<BookingTrendPointDto>> GetBookingTrendAsync(DateTime? fromUtc = null, DateTime? toUtc = null, string groupBy = "day");
+        Task<IReadOnlyList<MonthlyAdminStatsDto>> GetMonthlyStatsAsync(int year);
     }
 }
