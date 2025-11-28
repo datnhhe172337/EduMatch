2327975d5c71edb35a7e786490261ef70cbb5760
hoangnm <nguyenminhhoangtitlal@gmail.com>
2025-11-28 19:23:54 +0700

    add MonthlyAdminStatsDto

---
+++ EduMatch.BusinessLogicLayer/DTOs/MonthlyAdminStatsDto.cs
@@ -0,0 +1,22 @@
+namespace EduMatch.BusinessLogicLayer.DTOs
+{
+    public class MonthlyAdminStatsDto
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public UserStatsDto Users { get; set; } = new();
+        public TutorStatsDto Tutors { get; set; } = new();
+        public BookingStatsDto Bookings { get; set; } = new();
+        public RefundStatsDto Refunds { get; set; } = new();
+        public ReportStatsDto Reports { get; set; } = new();
+        public MonthlyRevenueStatsDto Revenue { get; set; } = new();
+    }
+
+    public class MonthlyRevenueStatsDto
+    {
+        public decimal GrossAmount { get; set; }
+        public decimal PlatformFeeAmount { get; set; }
+        public decimal TutorPayoutAmount { get; set; }
+        public decimal RefundedAmount { get; set; }
+    }
+}
