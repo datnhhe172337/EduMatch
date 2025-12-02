1d8f93a313e0234b215bed915091f2bf32b7b528
vnq03 <vietnq021103@gmail.com>
2025-12-02 16:41:09 +0700

    fix bug

--- EduMatch.UnitTests/TutorApplicationUnitTests.cs
+++ EduMatch.UnitTests/TutorApplicationUnitTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using EduMatch.BusinessLogicLayer.Services;
+using EduMatch.DataAccessLayer.Entities;
+using EduMatch.DataAccessLayer.Enum;
+using EduMatch.DataAccessLayer.Interfaces;
+using EduMatch.UnitTests.FakeData;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +14,76 @@ namespace EduMatch.UnitTests
 {
     public class TutorApplicationUnitTests
     {
+        private readonly Mock<ITutorApplicationRepository> _repoMock;
+        private readonly TutorApplicationService _service;
 
+        public TutorApplicationUnitTests()
+        {
+            _repoMock = new Mock<ITutorApplicationRepository>();
+            _service = new TutorApplicationService(_repoMock.Object, new FakeEmailService());
+        }
+
+        [Fact]
+        public async Task TutorApply_Throws_WhenClassRequestNotFound_UnitTest()
+        {
+            _repoMock.Setup(r => r.GetClassRequestByIdAsync(1)).ReturnsAsync((ClassRequest)null);
+
+            await Assert.ThrowsAsync<KeyNotFoundException>(() =>
+                _service.TutorApplyAsync(1, "tutor@gmail.com", "I want to apply"));
+        }
+
+        [Fact]
+        public async Task TutorApply_Throws_WhenClassRequestNotOpen_UnitTest()
+        {
+            var classRequest = new ClassRequest { Id = 1, Status = ClassRequestStatus.Pending };
+            _repoMock.Setup(r => r.GetClassRequestByIdAsync(1)).ReturnsAsync(classRequest);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() =>
+                _service.TutorApplyAsync(1, "tutor@gmail.com", "I want to apply"));
+        }
+
+        [Fact]
+        public async Task TutorApply_Throws_WhenTutorNotFound_UnitTest()
+        {
+            var classRequest = new ClassRequest { Id = 1, Status = ClassRequestStatus.Open };
+            _repoMock.Setup(r => r.GetClassRequestByIdAsync(1)).ReturnsAsync(classRequest);
+            _repoMock.Setup(r => r.GetTutorByEmailAsync("tutor@gmail.com")).ReturnsAsync((TutorProfile)null);
+
+            await Assert.ThrowsAsync<Exception>(() =>
+                _service.TutorApplyAsync(1, "tutor@gmail.com", "I want to apply"));
+        }
+
+        [Fact]
+        public async Task TutorApply_Throws_WhenTutorAlreadyApplied_UnitTest()
+        {
+            var classRequest = new ClassRequest { Id = 1, Status = ClassRequestStatus.Open };
+            var tutor = new TutorProfile { Id = 10, UserEmail = "tutor@gmail.com" };
+
+            _repoMock.Setup(r => r.GetClassRequestByIdAsync(1)).ReturnsAsync(classRequest);
+            _repoMock.Setup(r => r.GetTutorByEmailAsync("tutor@gmail.com")).ReturnsAsync(tutor);
+            _repoMock.Setup(r => r.HasAppliedAsync(1, tutor.Id)).ReturnsAsync(true);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() =>
+                _service.TutorApplyAsync(1, "tutor@gmail.com", "I want to apply"));
+        }
+
+        [Fact]
+        public async Task TutorApply_Success_UnitTest()
+        {
+            var classRequest = new ClassRequest { Id = 1, Status = ClassRequestStatus.Open };
+            var tutor = new TutorProfile { Id = 10, UserEmail = "tutor@gmail.com" };
+
+            _repoMock.Setup(r => r.GetClassRequestByIdAsync(1)).ReturnsAsync(classRequest);
+            _repoMock.Setup(r => r.GetTutorByEmailAsync("tutor@gmail.com")).ReturnsAsync(tutor);
+            _repoMock.Setup(r => r.HasAppliedAsync(1, tutor.Id)).ReturnsAsync(false);
+            _repoMock.Setup(r => r.AddApplicationAsync(It.IsAny<TutorApplication>()))
+                     .Returns(Task.CompletedTask);
+
+            await _service.TutorApplyAsync(1, "tutor@gmail.com", "I want to apply");
+
+            _repoMock.Verify(r => r.AddApplicationAsync(It.Is<TutorApplication>(
+                a => a.ClassRequestId == 1 && a.TutorId == 10 && a.Message == "I want to apply"
+            )), Times.Once);
+        }
     }
 }
